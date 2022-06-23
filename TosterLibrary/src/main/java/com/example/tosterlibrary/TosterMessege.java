452419c54a3ccb19cff13dc1e479505e549f9617
Senthuran.Thanabalas <senthu0207@gmail.com>
2022-06-23 10:12:43 +0530

    inital commit

---
+++ TosterLibrary/src/main/java/com/example/tosterlibrary/TosterMessege.java
@@ -0,0 +1,12 @@
+package com.example.tosterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class TosterMessege {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
